5bf5880273fa8925b8c6240505b847f441177369
@u9KqBJdRPsTFXT <legalcoa668@gmail.com>
2023-11-19 22:55:36 +0600

    working

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -5,6 +5,5 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
   port: process.env.PORT,
-
   database_url: process.env.DATABASE_URL,
 };
---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,39 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type Username = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: Username;
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'inactive';
+};
