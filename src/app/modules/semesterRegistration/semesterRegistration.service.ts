818dd45d8560a25897a50654dd1ad248e7fd8c3a
gsjoy24 <gour.joy24@gmail.com>
2023-12-15 03:50:33 +0600

    working on service

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,9 +1,32 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TSemesterRegistration } from './semesterRegistration.interface';
 import { SemesterRegistration } from './semesterRegistration.model';
 
 const createSemesterRegistrationIntoDB = async (
   payload: TSemesterRegistration,
 ) => {
+  const academicSemester = payload?.academicSemester;
+
+  const isSemesterRegistrationAlreadyExist = await SemesterRegistration.findOne(
+    {
+      academicSemester,
+    },
+  );
+  if (!isSemesterRegistrationAlreadyExist) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      'Semester registration already exist',
+    );
+  }
+  // check if academic semester exist
+  const isAcademicSemesterExists =
+    await AcademicSemester.findById(academicSemester);
+  if (!isAcademicSemesterExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Academic semester not found');
+  }
+
   const semesterRegistration = await SemesterRegistration.create(payload);
   return semesterRegistration;
 };
