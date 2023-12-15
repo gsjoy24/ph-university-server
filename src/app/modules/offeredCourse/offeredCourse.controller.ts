76e126e01776bec89110d2b3329fb18a9aa94021
gsjoy24 <gour.joy24@gmail.com>
2023-12-15 19:51:32 +0600

    added service and controller for Offered Course

---
+++ src/app/modules/offeredCourse/offeredCourse.controller.ts
@@ -0,0 +1,72 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import OfferedCourseServices from './offeredCourse.service';
+
+const createOfferedCourse = catchAsync(async (req, res) => {
+  const result = await OfferedCourseServices.createOfferedCourseIntoDB(
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course created successfully!',
+    data: result,
+  });
+});
+
+const getAllOfferedCourses = catchAsync(async (req, res) => {
+  const result = await OfferedCourseServices.getAllOfferedCourseFromDB();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Courses data retrieved successfully!',
+    data: result,
+  });
+});
+
+const getSingleOfferedCourse = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await OfferedCourseServices.getSingleOfferedCourseFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course retrieved successfully!',
+    data: result,
+  });
+});
+
+const updateOfferedCourse = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await OfferedCourseServices.updateOfferedCourseIntoDB(
+    id,
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course updated successfully!',
+    data: result,
+  });
+});
+
+const deleteOfferedCourse = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await OfferedCourseServices.deleteOfferedCourseFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course deleted successfully!',
+    data: result,
+  });
+});
+
+const OfferedCourseControllers = {
+  createOfferedCourse,
+  getAllOfferedCourses,
+  getSingleOfferedCourse,
+  updateOfferedCourse,
+  deleteOfferedCourse,
+};
+
+export default OfferedCourseControllers;
---
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -0,0 +1,42 @@
+import { TOfferedCourse } from './offeredCourse.interface';
+import { OfferedCourse } from './offeredCourse.model';
+
+const createOfferedCourseIntoDB = async (courseData: TOfferedCourse) => {
+  const result = await OfferedCourse.create(courseData);
+  return result;
+};
+
+const getAllOfferedCourseFromDB = async () => {
+  const result = await OfferedCourse.find();
+  return result;
+};
+
+const getSingleOfferedCourseFromDB = async (id: string) => {
+  const result = await OfferedCourse.findById(id);
+  return result;
+};
+
+const updateOfferedCourseIntoDB = async (
+  id: string,
+  payload: Partial<TOfferedCourse>,
+) => {
+  const result = await OfferedCourse.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
+  return result;
+};
+
+const deleteOfferedCourseFromDB = async (id: string) => {
+  const result = await OfferedCourse.findByIdAndDelete(id);
+  return result;
+};
+
+const OfferedCourseServices = {
+  createOfferedCourseIntoDB,
+  getAllOfferedCourseFromDB,
+  getSingleOfferedCourseFromDB,
+  updateOfferedCourseIntoDB,
+  deleteOfferedCourseFromDB,
+};
+
+export default OfferedCourseServices;
