f957d13ce7b43f06d0cb3542d1b3f0e387c8361a
gsjoy24 <gour.joy24@gmail.com>
2023-12-15 19:09:54 +0600

    added OfferedCourse module

---
+++ src/app/modules/offeredCourse/offeredCourse.constant.ts
@@ -0,0 +1,1 @@
+export const Days = ['sat', 'sun', 'mon', 'tue', 'wed', 'thu', 'fri'];
---
+++ src/app/modules/offeredCourse/offeredCourse.model.ts
@@ -0,0 +1,67 @@
+import { Schema, model } from 'mongoose';
+import { TOfferedCourse } from './offeredCourse.interface';
+import { Days } from './offeredCourse.constant';
+
+const OfferCourseSchema = new Schema<TOfferedCourse>(
+  {
+    semesterRegistration: {
+      type: Schema.Types.ObjectId,
+      ref: 'SemesterRegistration',
+      required: [true, 'Semester registration is required'],
+    },
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicSemester',
+      required: [true, 'Academic semester is required'],
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
+      required: [true, 'Academic faculty is required'],
+    },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicDepartment',
+      required: [true, 'Academic department is required'],
+    },
+    course: {
+      type: Schema.Types.ObjectId,
+      ref: 'Course',
+      required: [true, 'Course is required'],
+    },
+    faculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'Faculty',
+      required: [true, 'Faculty is required'],
+    },
+    maxCapacity: {
+      type: Number,
+      required: [true, 'Max capacity is required'],
+    },
+    section: {
+      type: String,
+      required: [true, 'Section is required'],
+    },
+    days: {
+      type: String,
+      enum: Days,
+      required: [true, 'Days is required'],
+    },
+    startTime: {
+      type: String,
+      required: [true, 'Start time is required'],
+    },
+    endTime: {
+      type: String,
+      required: [true, 'End time is required'],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const OfferedCourse = model<TOfferedCourse>(
+  'OfferedCourse',
+  OfferCourseSchema,
+);
