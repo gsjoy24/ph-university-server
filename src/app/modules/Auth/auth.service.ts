b97c66820c419acaaf53bc22c024bad8ec624203
gsjoy24 <gour.joy24@gmail.com>
2023-12-18 01:17:31 +0600

    working on validation on auth

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,12 +1,38 @@
-import { TLoginUser } from "./auth.interface";
+import bcrypt from 'bcrypt';
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { User } from '../user/user.model';
+import { TLoginUser } from './auth.interface';
 
-const loginUser = (payload: TLoginUser) => {
+const loginUser = async (payload: TLoginUser) => {
+  const { id, password } = payload;
 
-   const { id, password } = payload;
-   
-   
-}
+  // check if the user is exist
+  const isUserExists = await User.findOne({ id });
+  if (!isUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found');
+  }
+
+  // check if the user is deleted
+  if (isUserExists.isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted');
+  }
+
+  // check if the user is blocked
+  if (isUserExists?.status === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked');
+  }
+
+  // check if the password is correct
+  const isPasswordMatch = await bcrypt.compare(
+    password,
+    isUserExists?.password,
+  );
+  if (!isPasswordMatch) {
+    throw new AppError(httpStatus.UNAUTHORIZED, 'Invalid credentials');
+  }
+};
 
 export const AuthServices = {
-   loginUser
-}
+  loginUser,
+};
