ace7d56e7f7ad18e881fe74be3254ad75f115051
gsjoy24 <gour.joy24@gmail.com>
2023-12-19 22:31:31 +0600

    added login route

--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import { AuthControllers } from './auth.controller';
+import { LoginUserValidationSchema } from './auth.validation';
+import validateRequest from '../../middlewares/validateRequest';
 
 const router = express.Router();
 
-router.post('/login', (req, res) => {});
+router.post(
+  '/login',
+  validateRequest(LoginUserValidationSchema),
+  AuthControllers.loginUser,
+);
 
 export const AuthRoutes = router;
