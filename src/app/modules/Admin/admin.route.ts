54105cf53e4e918227703a9fcdd7a5c1efbbe72c
gsjoy24 <gour.joy24@gmail.com>
2023-12-21 22:55:14 +0600

    updated AdminRoutes

--- src/app/modules/Admin/admin.route.ts
+++ src/app/modules/Admin/admin.route.ts
@@ -2,19 +2,26 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { AdminControllers } from './admin.controller';
 import { updateAdminValidationSchema } from './admin.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLES } from '../user/user.constant';
 
 const router = express.Router();
 
-router.get('/', AdminControllers.getAllAdmins);
+router.get('/', auth(USER_ROLES.admin), AdminControllers.getAllAdmins);
 
-router.get('/:id', AdminControllers.getSingleAdmin);
+router.get('/:id', auth(USER_ROLES.admin), AdminControllers.getSingleAdmin);
 
 router.patch(
   '/:id',
+  auth(USER_ROLES.admin),
   validateRequest(updateAdminValidationSchema),
   AdminControllers.updateAdmin,
 );
 
-router.delete('/:adminId', AdminControllers.deleteAdmin);
+router.delete(
+  '/:adminId',
+  auth(USER_ROLES.admin),
+  AdminControllers.deleteAdmin,
+);
 
 export const AdminRoutes = router;
