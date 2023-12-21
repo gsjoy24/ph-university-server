7dea95d5e8f6ded22e70ca31f7d4479037396794
gsjoy24 <gour.joy24@gmail.com>
2023-12-21 22:56:58 +0600

    updated FacultyRoutes

--- src/app/modules/Faculty/faculty.route.ts
+++ src/app/modules/Faculty/faculty.route.ts
@@ -2,19 +2,19 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { FacultyControllers } from './faculty.controller';
 import { updateFacultyValidationSchema } from './faculty.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLES } from '../user/user.constant';
 
 const router = express.Router();
 
-router.get('/:id', FacultyControllers.getSingleFaculty);
-
+router.get('/:id', auth(USER_ROLES.admin), FacultyControllers.getSingleFaculty);
 router.patch(
   '/:id',
+  auth(USER_ROLES.admin),
   validateRequest(updateFacultyValidationSchema),
   FacultyControllers.updateFaculty,
 );
-
-router.delete('/:id', FacultyControllers.deleteFaculty);
-
-router.get('/', FacultyControllers.getAllFaculties);
+router.delete('/:id', auth(USER_ROLES.admin), FacultyControllers.deleteFaculty);
+router.get('/', auth(USER_ROLES.admin), FacultyControllers.getAllFaculties);
 
 export const FacultyRoutes = router;
