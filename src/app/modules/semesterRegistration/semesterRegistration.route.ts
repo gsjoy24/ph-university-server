2a64e3089e94eff100376df4767367ac0350f279
gsjoy24 <gour.joy24@gmail.com>
2023-12-21 22:59:16 +0600

    updated SemesterRegistrationRoutes

--- src/app/modules/semesterRegistration/semesterRegistration.route.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -2,22 +2,31 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { semesterRegistrationControllers } from './semesterRegistration.controller';
 import { semesterRegistrationValidations } from './semesterRegistration.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLES } from '../user/user.constant';
 const route = express.Router();
 
 route.post(
   '/',
+  auth(USER_ROLES.admin),
   validateRequest(
     semesterRegistrationValidations.createSemesterRegistrationValidationSchema,
   ),
   semesterRegistrationControllers.createSemesterRegistration,
 );
-route.get('/', semesterRegistrationControllers.getAllSemesterRegistration);
+route.get(
+  '/',
+  auth(USER_ROLES.admin),
+  semesterRegistrationControllers.getAllSemesterRegistration,
+);
 route.get(
   '/:id',
+  auth(USER_ROLES.admin),
   semesterRegistrationControllers.getSingleSemesterRegistration,
 );
 route.patch(
   '/:id',
+  auth(USER_ROLES.admin),
   validateRequest(
     semesterRegistrationValidations.updateSemesterRegistrationValidationSchema,
   ),
