d21fad6cd591ca461995d6065bb9607555a6528b
gsjoy24 <gour.joy24@gmail.com>
2023-12-21 23:01:50 +0600

    updated StudentRoutes

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -2,15 +2,18 @@ import express from 'express';
 import { StudentControllers } from './student.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { studentValidations } from './student.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLES } from '../user/user.constant';
 const route = express.Router();
 
-route.get('/', StudentControllers.getAllStudent);
-route.get('/:id', StudentControllers.getSingleStudent);
+route.get('/', auth(USER_ROLES.admin), StudentControllers.getAllStudent);
+route.get('/:id', auth(USER_ROLES.admin), StudentControllers.getSingleStudent);
 route.patch(
   '/:id',
+  auth(USER_ROLES.admin),
   validateRequest(studentValidations.updateStudentValidationSchema),
   StudentControllers.updateStudent,
 );
-route.delete('/:id', StudentControllers.deleteStudent);
+route.delete('/:id', auth(USER_ROLES.admin), StudentControllers.deleteStudent);
 
 export const StudentRoutes = route;
