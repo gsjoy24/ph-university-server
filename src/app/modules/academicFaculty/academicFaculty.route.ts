9b0b399aefabc2acaeac62c9684aedf39a69b05c
gsjoy24 <gour.joy24@gmail.com>
2023-12-21 22:52:18 +0600

    updated AcademicFacultyRoutes

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -2,22 +2,38 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicFacultyControllers } from './academicFaculty.controller';
 import { AcademicFacultyValidations } from './academicFaculty.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLES } from '../user/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/',
+  auth(USER_ROLES.admin),
   validateRequest(AcademicFacultyValidations.academicFacultyValidationSchema),
   AcademicFacultyControllers.createAcademicFaculty,
 );
 
-router.get('/', AcademicFacultyControllers.getAllAcademicFaculties);
-router.get('/:id', AcademicFacultyControllers.getSingleAcademicFaculty);
+router.get(
+  '/',
+  auth(USER_ROLES.admin, USER_ROLES.faculty),
+  AcademicFacultyControllers.getAllAcademicFaculties,
+);
+router.get(
+  '/:id',
+  auth(USER_ROLES.admin, USER_ROLES.faculty),
+  AcademicFacultyControllers.getSingleAcademicFaculty,
+);
 router.patch(
   '/:id',
+  auth(USER_ROLES.admin),
   validateRequest(AcademicFacultyValidations.academicFacultyValidationSchema),
   AcademicFacultyControllers.updateAcademicFaculty,
 );
-router.delete('/:id', AcademicFacultyControllers.deleteAcademicFaculty);
+router.delete(
+  '/:id',
+  auth(USER_ROLES.admin),
+  AcademicFacultyControllers.deleteAcademicFaculty,
+);
 
 export const AcademicFacultyRoutes = router;
