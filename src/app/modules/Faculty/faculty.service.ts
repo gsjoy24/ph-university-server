7161e07f8808e375a3adbc5ec63aca23a9b34363
gsjoy24 <gour.joy24@gmail.com>
2024-02-06 03:16:27 +0600

    Update faculty service to populate academicFaculty

--- src/app/modules/Faculty/faculty.service.ts
+++ src/app/modules/Faculty/faculty.service.ts
@@ -10,7 +10,7 @@ import { Faculty } from './faculty.model';
 
 const getAllFacultiesFromDB = async (query: Record<string, unknown>) => {
   const facultyQuery = new QueryBuilder(
-    Faculty.find().populate('academicDepartment'),
+    Faculty.find().populate('academicDepartment academicFaculty'),
     query,
   )
     .search(FacultySearchableFields)
@@ -25,7 +25,9 @@ const getAllFacultiesFromDB = async (query: Record<string, unknown>) => {
 };
 
 const getSingleFacultyFromDB = async (id: string) => {
-  const result = await Faculty.findById(id).populate('academicDepartment');
+  const result = await Faculty.findById(id).populate(
+    'academicDepartment academicFaculty',
+  );
 
   if (!result) {
     throw new AppError(httpStatus.NOT_FOUND, 'Faculty not found');
